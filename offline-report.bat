a4d24fdc69862ce833b2eec00b0274880c0c583f
Giacomo <giacomo.redjack@gmail.com>
2021-08-18 15:40:20 +0200

    Cambiata la cartella a cui salva il report

--- offline-report.bat
+++ offline-report.bat
@@ -4,15 +4,16 @@ set PATH_JACOCO_CLI_JAR="jacococli"
 set PATH_JCS_SRC="jcs_source\java"
 set PATH_JCS_JAR="jcs_jar"
 set PATH_JCS_FAT_JAR="jcs_fat_jar"
+set PATH_REPORT="target\site\jacoco"
 
 rem 2) CREAZIONE CON JACOCO DA CLI DEL REPORT
 rem (https://www.jacoco.org/jacoco/trunk/doc/cli.html)
 
 rem la cartella corrispondente in target
-mkdir target\jacoco-gen\jcs-coverage\
+mkdir %PATH_REPORT%
 
 rem move jacoco.exec target\jacoco.exec
 
-java -jar %PATH_JACOCO_CLI_JAR%\jacococli.jar report target\jacoco.exec --classfiles %PATH_JCS_JAR%\jcs-1.3.jar --sourcefiles %PATH_JCS_SRC% --html target\jacoco-gen\jcs-coverage\ --xml target\jacoco-gen\jcs-coverage\file.xml --csv target\jacoco-gen\jcs-coverage\file.csv
+java -jar %PATH_JACOCO_CLI_JAR%\jacococli.jar report target\jacoco.exec --classfiles %PATH_JCS_JAR%\jcs-1.3.jar --sourcefiles %PATH_JCS_SRC% --html %PATH_REPORT% --xml %PATH_REPORT%\jacoco.xml --csv %PATH_REPORT%\jacoco.csv
 
-rem java -jar jacoco\lib\jacococli.jar report target\jacoco.exec --classfiles jcs_jar\jcs-1.3.jar --sourcefiles jcs_source\java --html target\jacoco-gen\jcs-coverage\ --xml target\jacoco-gen\jcs-coverage\file.xml --csv target\jacoco-gen\jcs-coverage\file.csv
+rem java -jar jacococli\jacococli.jar report target\jacoco.exec --classfiles jcs_jar\jcs-1.3.jar --sourcefiles jcs_source\java --html target\site\jacoco\ --xml target\\site\jacoco\jacoco.xml --csv target\site\jacoco\jacoco.csv
