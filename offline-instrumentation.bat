02e1ceab4954ddeff8bd1a6c39f9434445948dd2
Giacomo <giacomo.redjack@gmail.com>
2021-08-18 15:18:51 +0200

    Spostato jacococli.jar Migliorati pom e bat

--- offline-instrumentation.bat
+++ offline-instrumentation.bat
@@ -1,13 +1,12 @@
 @echo off
 
-set PATH_JACOCO_CLI_JAR="jacoco\lib"
+set PATH_JACOCO_CLI_JAR="jacococli"
 set PATH_JCS_JAR="jcs_jar"
 set PATH_JCS_FAT_JAR="jcs_fat_jar"
 
 rem CREAZIONE CON JACOCO DA CLI DEL FAT-JAR INSTUMENTATO:
 rem (https://www.jacoco.org/jacoco/trunk/doc/cli.html)
 
-java -jar ${PATH_JACOCO_CLI_JAR}\jacococli.jar instrument ${PATH_JCS_JAR}\jcs-1.3.jar --dest ${PATH_JCS_FAT_JAR}
+java -jar %PATH_JACOCO_CLI_JAR%\jacococli.jar instrument %PATH_JCS_JAR%\jcs-1.3.jar --dest %PATH_JCS_FAT_JAR%
 
 rem java -jar jacoco\lib\jacococli.jar instrument jcs_jar\jcs-1.3.jar --dest jcs_fat_jar\
-
--- offline-report.bat
+++ offline-report.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set PATH_JACOCO_CLI_JAR="jacoco\lib"
+set PATH_JACOCO_CLI_JAR="jacococli"
 set PATH_JCS_SRC="jcs_source\java"
 set PATH_JCS_JAR="jcs_jar"
 set PATH_JCS_FAT_JAR="jcs_fat_jar"
@@ -11,7 +11,7 @@ rem (https://www.jacoco.org/jacoco/trunk/doc/cli.html)
 rem la cartella corrispondente in target
 mkdir target\jacoco-gen\jcs-coverage\
 
-move jacoco.exec target\jacoco.exec
+rem move jacoco.exec target\jacoco.exec
 
 java -jar %PATH_JACOCO_CLI_JAR%\jacococli.jar report target\jacoco.exec --classfiles %PATH_JCS_JAR%\jcs-1.3.jar --sourcefiles %PATH_JCS_SRC% --html target\jacoco-gen\jcs-coverage\ --xml target\jacoco-gen\jcs-coverage\file.xml --csv target\jacoco-gen\jcs-coverage\file.csv
 
