2468f081bbd1450ba9f864939786c20d061b9dce
Giacomo <giacomo.redjack@gmail.com>
2021-08-18 17:05:45 +0200

    Aggiunta classe dummy

--- src/main/java/org/giacomolorenzo/rossi/Dummy.java
+++ src/main/java/org/giacomolorenzo/rossi/Dummy.java
@@ -1,7 +1,9 @@
 package org.giacomolorenzo.rossi;
 
+import java.util.logging.Logger;
+
 public class Dummy {
     public static void main(String[] args) {
-        System.out.println("Hello Dummy!");
+        Logger.getAnonymousLogger().info("Hello Dummy!");
     }
 }
