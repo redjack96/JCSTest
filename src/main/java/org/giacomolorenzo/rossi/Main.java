937a8f076042409babd42923290672d7ee9e0931
Giacomo <giacomo.redjack@gmail.com>
2021-08-19 14:14:23 +0200

    Rimessa la classe...

---
+++ src/main/java/org/giacomolorenzo/rossi/Main.java
@@ -0,0 +1,9 @@
+package org.giacomolorenzo.rossi;
+
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger.getAnonymousLogger().info("CIAO");
+    }
+}
