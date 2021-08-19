19fa909577282def4e6cef81e8edb7614659850a
Giacomo <giacomo.redjack@gmail.com>
2021-08-19 14:11:28 +0200

    Copiati file .class per mostrarli nella coverage di SonarCLoud

--- src/main/java/org/giacomolorenzo/rossi/Dummy.java
+++
@@ -1,9 +0,0 @@
-package org.giacomolorenzo.rossi;
-
-import java.util.logging.Logger;
-
-public class Dummy {
-    public static void main(String[] args) {
-        Logger.getAnonymousLogger().info("Hello Dummy!");
-    }
-}
